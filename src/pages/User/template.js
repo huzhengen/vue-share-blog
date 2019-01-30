40237237455dd56db3755def784288dfa05e718e
卢方 <huzhengen@163.com>
2019-01-30 18:11:14 +0800

    用户页面

--- src/pages/User/template.js
+++ src/pages/User/template.js
@@ -1,5 +1,46 @@
+import blog from '@/api/blog.js'
+
 export default {
-    props: {
-        msg: String
+    data() {
+        return {
+            blogs: [],
+            user: {},
+            page: 1,
+            total: 0,
+        }
+    },
+    created() {
+        this.userId = this.$route.params.userId
+        this.page = this.$route.query.page || 1
+        blog.getBlogsByUserId(this.userId, { page: this.page }).then(res => {
+            this.blogs = res.data
+            this.page = res.page
+            this.total = res.total
+            if (res.data.length > 0) {
+                this.user = res.data[0].user
+            }
+        })
+    },
+    methods: {
+        splitDate(dateStr) {
+            let dateObj = typeof dateStr === 'object' ? dateStr : new Date(dateStr)
+            return {
+                date: dateObj.getDate(),
+                month: dateObj.getMonth() + 1,
+                year: dateObj.getFullYear()
+            }
+        },
+        onPageChange(newPage) {
+            console.log(newPage)
+            blog.getBlogsByUserId(this.userId, { page: newPage }).then(res => {
+                this.blogs = res.data
+                this.page = res.page
+                this.total = res.total
+                if (res.data.length > 0) {
+                    this.user = res.data[0].user
+                }
+                this.$router.push({ path: `/user/${this.userId}`, query: { page: newPage } })
+            })
+        },
     }
 };
