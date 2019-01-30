e88e4bf9fbddc24ad385639ad60d88fada8a2bc7
卢方 <huzhengen@163.com>
2019-01-30 17:27:09 +0800

    发布时间

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router/index.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store/index.js'
+import Util from './helpers/util.js'
 
 Vue.use(ElementUI)
+Vue.use(Util)
 Vue.config.productionTip = false
 
 new Vue({
