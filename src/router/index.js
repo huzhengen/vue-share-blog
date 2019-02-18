4c748677c90f7d8f3ea9ef62a8cf6632a63ef05d
卢方 <huzhengen@163.com>
2019-02-18 14:00:15 +0800

    开启gzip

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Router from 'vue-router'
 // import User from '../pages/User/template.vue'
 // import My from '../pages/My/template.vue'
 // import Edit from '../pages/Edit/template.vue'
+
 const Index = () => import('@/pages/Index/template.vue')
 const Login = () => import('@/pages/Login/template.vue')
 const Register = () => import('@/pages/Register/template.vue')
@@ -17,6 +18,15 @@ const Create = () => import('@/pages/Create/template.vue')
 const User = () => import('@/pages/User/template.vue')
 const My = () => import('@/pages/My/template.vue')
 const Edit = () => import('@/pages/Edit/template.vue')
+
+// const Index = resolve => require(['@/pages/Index/template.vue'], resolve)
+// const Login = resolve => require(['@/pages/Login/template.vue'], resolve)
+// const Register = resolve => require(['@/pages/Register/template.vue'], resolve)
+// const Detail = resolve => require(['@/pages/Detail/template.vue'], resolve)
+// const Create = resolve => require(['@/pages/Create/template.vue'], resolve)
+// const User = resolve => require(['@/pages/User/template.vue'], resolve)
+// const My = resolve => require(['@/pages/My/template.vue'], resolve)
+// const Edit = resolve => require(['@/pages/Edit/template.vue'], resolve)
 
 import store from '../store/index.js'
 
